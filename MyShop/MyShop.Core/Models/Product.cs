30aa35791d87eb9f6da18beda0d37f68d7694ae9
raja83520 <ch.rajas@gmail.com>
2020-04-28 15:12:31 -0400

    Implemented generic Repository

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -8,22 +8,16 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public string Id { get; set; }
         [StringLength(20)]
         [DisplayName("Product Name")]
         public string Name { get; set; }
         public string Description { get; set; }
-        [Range(0,1000)]
+        [Range(0, 1000)]
         public string Price { get; set; }
         public string Category { get; set; }
-        public string Iamge { get; set; } 
+        public string Iamge { get; set; }
 
-
-       public Product()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
     }
 }
