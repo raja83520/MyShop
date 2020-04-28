412a6ab8b15773caaba6dd0643acabaa420fc017
raja83520 <ch.rajas@gmail.com>
2020-04-28 15:35:44 -0400

    unity

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T:BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
@@ -20,9 +21,7 @@ namespace MyShop.DataAccess.InMemory
             items = cache[className] as List<T>;
 
             if (items == null)
-            {
                 items = new List<T>();
-            }
         }
 
         public void Commit()
@@ -47,7 +46,7 @@ namespace MyShop.DataAccess.InMemory
         {
             T t = items.Find(i => i.Id == Id);
             if (t != null)
-               return t;
+                return t;
             else
                 throw new Exception(className + "Not Found");
         }
@@ -55,7 +54,7 @@ namespace MyShop.DataAccess.InMemory
         public IQueryable<T> Collection()
         {
             return items.AsQueryable();
-                    }
+        }
 
         public void Delete(String Id)
         {
--- MyShop/MyShop.WebUI/Controllers/ProductMAnagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductMAnagerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
@@ -11,19 +12,19 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        InMemoryRepository<Product> context;
-        InMemoryRepository<ProductCategory> productCategories;
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
 
-        public ProductManagerController()
+        public ProductManagerController(IRepository<Product> productcontext, IRepository<ProductCategory> productCategoryContext)
         {
-            context = new InMemoryRepository<Product>();
-            productCategories = new InMemoryRepository<ProductCategory>();
+            context = productcontext;
+            productCategories = productCategoryContext;
         }
         // GET: ProductMAnager
         public ActionResult Index()
-        {     
+        {
             List<Product> products = context.Collection().ToList();
-                        return View(products);
+            return View(products);
         }
 
         public ActionResult Create()
@@ -31,7 +32,7 @@ namespace MyShop.WebUI.Controllers
             ProductManagerViewModel viewModel = new ProductManagerViewModel();
             viewModel.Product = new Product();
             viewModel.ProductCategories = productCategories.Collection();
-             
+
             return View(viewModel);
         }
         [HttpPost]
@@ -58,7 +59,7 @@ namespace MyShop.WebUI.Controllers
                 ProductManagerViewModel viewModel = new ProductManagerViewModel();
                 viewModel.Product = product;
                 viewModel.ProductCategories = productCategories.Collection();
-               
+
                 return View(viewModel);
             }
         }
