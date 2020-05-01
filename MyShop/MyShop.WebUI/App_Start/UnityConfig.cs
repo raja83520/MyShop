ab59550085cdb4bc568d96c2a29fade8d2e487ed
raja83520 <ch.rajas@gmail.com>
2020-05-01 12:16:42 -0400

    Implemeted SQL Repository

--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
+using MyShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -47,8 +48,8 @@ namespace MyShop.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
 
 
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository <Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
 
         }
     }
