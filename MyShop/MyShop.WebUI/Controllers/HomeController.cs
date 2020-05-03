c2c65607292cf3b6240fd90a23e6881307deaa65
raja83520 <ch.rajas@gmail.com>
2020-05-03 18:51:29 -0400

    proudct filter

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.ViewModels;
 
 namespace MyShop.WebUI.Controllers
 {
@@ -18,12 +19,22 @@ namespace MyShop.WebUI.Controllers
             context = productcontext;
             productCategories = productCategoryContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string Category = null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
-        }
+            List<Product> products;//= context.Collection().ToList();
+            List<ProductCategory> categories = productCategories.Collection().ToList();
 
+            if (Category == null)
+                products = context.Collection().ToList();
+            else
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
+        }
         public ActionResult Details(string Id)
         {
             Product product = context.Find(Id);
