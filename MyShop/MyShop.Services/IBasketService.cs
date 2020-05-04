139ae60bd8c21f01eb3a62eee92c97a7d922769a
raja83520 <ch.rajas@gmail.com>
2020-05-03 22:51:24 -0400

    Added baskemodel

---
+++ MyShop/MyShop.Services/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using MyShop.Core.ViewModels;
+using System.Collections.Generic;
+using System.Web;
+
+namespace MyShop.Services
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+    }
+}
