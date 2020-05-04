d3d43a1f0d8d770bd8723a1c24aad7f966f1bce2
raja83520 <ch.rajas@gmail.com>
2020-05-03 22:28:15 -0400

    added service

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -10,12 +10,14 @@ namespace MyShop.DataAccess.SQL
 {
     public class DataContext : DbContext
     {
-        public DataContext() 
+        public DataContext()
             : base("DefaultConnection")
         {
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
